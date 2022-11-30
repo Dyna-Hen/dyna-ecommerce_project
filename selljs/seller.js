f1e7bfef1ed5b045d8583537dc3373e57317ba59
ileanan <dynahen133@gmail.com>
2022-11-30 16:09:48 +0700

    [UPDATE PRODUCT]

--- selljs/seller.js
+++ selljs/seller.js
@@ -1,24 +1,4 @@
 
-// const dom_item_dialog = document.getElementById("item-dialog");
-// function showitem(element){
-//     element.style.display = "block";
-// }
-// function hideitem(element){
-//     element.style.display = "none";
-// }
-// function addQuestion(){
-//     showitem(dom_item_dialog);
-// }
-// function Cancel(){
-//     hideitem(dom_item_dialog);
-// }
-// function Create(){
-//     var itemname = document.getElementById("name").value;
-//     localStorage.setItem("textvalues",itemname);
-//     return false;
-// }
-
-// pppp
 // DOMS ELEMENTS  ---------------------------------------------------------
 const product_veiw = document.querySelector("#table");
 let add_product_form = document.getElementById("product-dialog");
@@ -66,7 +46,7 @@ function loadProduct() {
   if (itemStorage !== null) {
     product = itemStorage;
   }
-  console.log(itemStorage)
+  // console.log(itemStorage)
 }
 
 function renderProduct(){
@@ -76,43 +56,7 @@ function renderProduct(){
   newcontainer.id = "product-list";
   product_veiw.appendChild(newcontainer);
 
-  // for (let pro of product){
 
-  //   let tr = document.createElement("tr");
-
-  //   let td1 = document.createElement("td");
-  //   td1.textContent = pro.name;
-
-  //   let tdN = document.createElement("td");
-  //   tdN.textContent = pro.nblist;
-  //   // add
-  //   // productContainer.appendChild()
-  //   let td2 = document.createElement("td");
-  //   td2.textContent = pro.price;
-    
-  //   let td3 = document.createElement("td");
-
-  //   let deletebtt = document.createElement("button");
-  //   deletebtt.id = "deletebtt";
-  //   deletebtt.addEventListener("click", removebtt)
-  //   deletebtt.textContent = "Delete";
-    
-
-  //   let editbtt = document.createElement("button");
-  //   editbtt.id = "editbtt";
-  //   editbtt.addEventListener("click", editButton)
-  //   editbtt.textContent = "Edit";
-    
-  //   td3.appendChild(deletebtt);
-  //   td3.appendChild(editbtt);
-
-  //   tr.appendChild(tdN);
-  //   tr.appendChild(td1);
-  //   tr.appendChild(td2);
-  //   tr.appendChild(td3);
-  //   newcontainer.appendChild(tr)
-  // }
-  // document.body.appendChild(newcontainer)
   for (let index = 0; index < product.length; index++){
     let pro = product[index];
 
@@ -151,22 +95,14 @@ function renderProduct(){
     tr.appendChild(td3);
     newcontainer.appendChild(tr)
   }
-  }
+}
 
 
 function Addproduct() {
   let pro = null;
   show(add_product_form);
-  // let name = document.getElementById("proname").value;
-  // let price = doc.value-from-input;
-  // let item =  {
-  //   name: name,
-  //   price: price,
-  // }
-
-  // product.append(item);
-  // saveProduct();
 }
+
 function removebtt(event) {
   let index = event.target.parentElement.parentElement.dataset.index;
   console.log(index)
@@ -180,22 +116,33 @@ function Cancel(e) {
 }
 
 function editButton(event) {
-  productToedit = event.target.dataset.index;
+  productToedit = event.target.parentElement.parentElement.dataset.index;
+  // productToedit.parseInt(productToedit);
+  // console.log(productToedit)
+  let editpro = product[productToedit];
+
+  document.getElementById("proname").value = editpro.name;
+  document.getElementById("price").value = editpro.price;
 
   show(add_product_form);
 }
 
 function Create() {
   hide(add_product_form);
+ if (productToedit !== null) {
+  let editproduct = product[productToedit];
+  editproduct.name = document.getElementById("proname").value;
+  editproduct.price = document.getElementById("price").value;
 
+ } else{
   let newproduct = {};
   newproduct.name = document.getElementById("proname").value;
   newproduct.price = document.getElementById("price").value;
   newproduct.nblist = document.getElementById("nblist").value;
-
   product.push(newproduct);
 
-
+ }
+  
   saveProduct();
   renderProduct();
 }
@@ -228,53 +175,3 @@ renderProduct();
 
 
 
-
-// let rIndex = document.getElementById("table");
-// // // saveProduct();
-// // loadProduct();
-// // renderProduct();
-// function addTableRow(){
-//   //get the table by id
-//   //create a new row and cellls
-//   //get value from input text
-//   //set the values into row cell
-//   // let table = document.getElementById("table");
-//   Product_name = document.getElementById("name").value;
-//   Product_price = document.getElementById("price").value;
-//   id_number = document.getElementById("id").value;
-
-//   selectRowToInput();
-  
-// }
-// function selectRowToInput(){
-//   // let rIndex = document.getElementById("table");
-//   // for (let i = 0; i<table.rows.length; i++){
-//   //   table.rows[i].onclick = function(){
-//   //     //get the selected row index
-//   //     rIndex = this.rowIndex;
-//   //     // console.log(rIndex);
-//   //   };
-//   // }
-// }
-// selectRowToInput();
-// function edit(){
-//   let Product_name = document.getElementById("name").value;
-//   Product_price = document.getElementById("price").value;
-//   id_number = document.getElementById("id").value;
-
-//   table.rows[rIndex].cells[0].innerHTML = Product_name;
-//   table.rows[rIndex].cells[1].innerHTML = Product_price;
-//   table.rows[rIndex].cells[2].innerHTML = id_number;
-
-// }
-
-// function remove(){
-//   table.deleteRow(rIndex);
-//   //clear input text
-//   document.getElementById("name").value = "";
-//   document.getElementById("price").value = "";
-//   document.getElementById("id").value = "";
-// }
-
-// saveProduct();
-// loadProduct();
